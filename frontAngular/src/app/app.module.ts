b786b84a8610005f6bc82d7796c7571da668cf99
Fabio <fabiomdg1@gmail.com>
2022-02-17 16:09:26 -0300

    Correção arquivo app.module

--- frontAngular/src/app/app.module.ts
+++ frontAngular/src/app/app.module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaCargoComponent } from './componentes/lista-cargo/lista-cargo.component';
-import { CadastroCargoComponent } from './componentes/cadastro-cargo/cadastro-cargo.component';
 import { EdicaoCargoComponent } from './componentes/edicao-cargo/edicao-cargo.component';
 import { ExclusaoCargoComponent } from './componentes/exclusao-cargo/exclusao-cargo.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { HeaderComponent } from './componentes/header/header.component';
+import { CadastroCargoComponent } from './componentes/cadastro-cargo/cadastro-cargo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaCargoComponent,
-    CadastroCargoComponent,
     EdicaoCargoComponent,
     ExclusaoCargoComponent,
     FooterComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    CadastroCargoComponent
   ],
   imports: [
     BrowserModule,
--- frontAngular/src/app/componentes/cadastro-cargo/cadastro-cargo.component.spec.ts
+++ frontAngular/src/app/componentes/cadastro-cargo/cadastro-cargo.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { CadastroCargoComponent } from './cadastro-cargo.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('CadastroCargoComponent', () => {
+  let component: CadastroCargoComponent;
+  let fixture: ComponentFixture<CadastroCargoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ CadastroCargoComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(CadastroCargoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
